73114a85105ed1b8047b86405ba5dd48114c45ce
Alex Zuniga <azuniga90@yahoo.com>
2014-10-10 01:50:16 +0000

    Added homepage and adjusted route to display accordingly. Input rough initiate code for stripe api. NOT functional at the moment but will be soon.

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,65 +1,66 @@
 <!DOCTYPE html>
-    <head>
-        <title>Stouffer Pay</title>
+<head>
+    <title>Stouffer Pay</title>
+    
+    <!-- Meta Tags -->
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Css Global Compulsory -->
+    <link rel="stylesheet" href="/assets/plugins/bootstrap/css/bootstrap.min.css">
+    <!-- Css Implementing Plugins -->
+    <link rel="stylesheet" href="/assets/plugins/font-awesome/css/font-awesome.min.css">
+    <!-- Css Theme -->
+    <link rel="stylesheet" href="#">
+    <link rel="stylesheet" href="#">
+    <!-- Web Fonts -->
+    <link rel="stylesheet" href="/assets/css/sp_custom.css">
+@yield('topscript')
+</head>
+<body>
+  <div class="header"> 
+      <h1>Stouffer Pay Online</h1>
+  </div>
 
-        <!-- Meta Tags -->
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Css Global Compulsory -->
-        <link rel="stylesheet" href="/assets/plugins/bootstrap/css/bootstrap.min.css">
-        <!-- Css Implementing Plugins -->
-        <link rel="stylesheet" href="/assets/plugins/font-awesome/css/font-awesome.min.css">
-        <link rel="stylesheet" href="/assets/plugins/lightbox/css/lightbox.css">
-        <!-- Css Theme -->
-        <link rel="stylesheet" href="#">
-        <link rel="stylesheet" href="#">
-        <!-- Web Fonts -->
-        <link rel="stylesheet" href="/assets/css/sp_custom.css">
-      @yield('topscript')
-    </head>
-    <body>
-    <div class="header"> 
-        <h1>Stouffer Pay Online</h1>
-    </div>
-    
-  <!-- Content -->
-  <!--/////////////////// -->
-        @yield('content')
-  <!--/////////////////// -->
-  
-  <!-- End Content  -->
-      <!-- Footer -->
-        <div id="footer">
-            <div class="container">
-                <div class="row">
-                    <!-- Copyright -->
-                    <div class="col-md-4 text-center">
-                        <p>Copyright 2014 © StoufferPay.com</p>
-                    </div>
-                    <!-- Footer Social Icons -->
-                    <div class="col-md-4 text-center">
-                        <a href="#"> 
-                            About Us
-                        </a>
-                    </div>
-                    <!-- Up Button -->
-                    <div class="col-md-4 back-to-top">
-                        <a href="#">
-                            <i class="fa fa-angle-up fa-2x pull-right"></i>
-                        </a>
-                    </div>
-                </div><!-- /row -->
-            </div><!-- /container -->
+<!-- Content -->
+<!--/////////////////// -->
+@yield('content')
+<!--/////////////////// -->
+<!-- End Content  -->
+
+<!-- Footer -->
+  <div id="footer">
+    <div class="container">
+      <div class="row">
+        <!-- Copyright -->
+        <div class="col-md-4 text-center">
+          <p>Copyright 2014 © StoufferPay.com</p>
         </div>
-        <!-- End Footer -->
-    <!-- Js Global Compulsory  -->
-        <script src="/assets/plugins/jquery-1.11.1.min.js"></script>
-        <script src="/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
-        <!-- Js Implementing Plugins  -->
-        <script src="/assets/plugins/moderniz.js"></script>
-        <!-- Js Theme  -->
-        <script src="/assets/js/app.js"></script>
-    @yield('bottomscript')  
-    </body>
+        <!-- Footer Social Icons -->
+        <div class="col-md-4 text-center">
+          <a href="#">About Us</a>
+        </div>
+        <!-- Up Button -->
+        <div class="col-md-4 back-to-top">
+          <a href="#">
+            <i class="fa fa-angle-up fa-2x pull-right"></i>
+          </a>
+        </div>
+      </div><!-- /row -->
+    </div><!-- /container -->
+  </div><!-- End Footer -->
+</body>
+<!-- Js Global Compulsory  -->
+<script src="/assets/plugins/jquery-1.11.1.min.js"></script>
+<script src="/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<!-- Js Implementing Plugins  -->
+<!-- Stripe JS -->
+<script src="https://checkout.stripe.com/checkout.js"></script>
+<script type="text/javascript">
+  // This identifies your website in the createToken call below
+  Stripe.setPublishableKey('pk_test_reAytqQbAhrNrHLUhozO3i9w');
+  // ...
+</script>
+<!-- Js Theme  -->
+<script src="/assets/js/app.js"></script>
+@yield('bottomscript')  
 </html>
-    
--- app/views/payHome.blade.php
+++ app/views/payHome.blade.php
@@ -2,7 +2,29 @@
 
 @section('content')
 
+<div class="container">
+  <button id="customButton">Purchase</button>
 
-<h1></h1>
+<script>
+  var handler = StripeCheckout.configure({
+    key: 'pk_test_reAytqQbAhrNrHLUhozO3i9w',
+    image: 'assets/images/logo.png',
+    token: function(token) {
+      // Use the token to create the charge with a server-side script.
+      // You can access the token ID with `token.id`
+    }
+  });
+
+  document.getElementById('customButton').addEventListener('click', function(e) {
+    // Open Checkout with further options
+    handler.open({
+      name: 'Stouffer Pay',
+      description: '2 widgets ($20.00)',
+      amount: 2000
+    });
+    e.preventDefault();
+  });
+</script>
+</div>
 
 @stop
